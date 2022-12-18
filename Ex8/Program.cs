d005b62861a106d4009e074f55d49e62c012c407
Alevtina Soloveva <111588554+Alevtina-Soloveva@users.noreply.github.com>
2022-12-18 14:01:28 +0500

    Delete Ex8 directory

--- Ex8/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿Console.Clear();
-Console.WriteLine("Write your number : ");
-int a = Convert.ToInt32(Console.ReadLine());
-if (a % 2 == 0)
-    Console.WriteLine("Yes");
-else if (a % 2 >= 0 || a % 2 <= 0)
-    Console.WriteLine("No");
