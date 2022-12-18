dc75a949c096e55179e126f3fb2c7fe599d567cf
unknown <happyjaskass@mail.ru>
2022-12-18 14:02:49 +0500

    Ex1 error

--- Ex1/Program.cs
+++ Ex1/Program.cs
@@ -1,1 +1,5 @@
-﻿Console.WriteLine("Hello, Alevtina!");
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Result: {a * a}");
+Console.WriteLine("1\n2\n3\n4\n5");    
