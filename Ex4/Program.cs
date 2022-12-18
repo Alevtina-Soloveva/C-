5572781d018c6625189979dcabf98663dee98f1b
unknown <happyjaskass@mail.ru>
2022-12-18 13:48:42 +0500

    Ex4 done

---
+++ Ex4/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+Console.WriteLine("Введите 1 число : ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 2 число : ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 3 число : ");
+int c = Convert.ToInt32(Console.ReadLine());
+if (a <= b && c <= b)
+    Console.WriteLine($"Большее число : {b}");
+else if (a >= b && a >= c)
+    Console.WriteLine($"Большее число : {a}");
+else if (c >= a && c >= b)
+    Console.WriteLine($"Большее число : {c}");
+else if (a == b && c >= a)
+    Console.WriteLine($"Большее число : {c}");
+else if (a == b && c <= a)
+    Console.WriteLine($"Большее число : {a}");
+else if (a == c && c <= b)
+    Console.WriteLine($"Большее число : {b}");
+else if (c == b && c <= a)
+    Console.WriteLine($"Большее число : {a}");
+else if (c == b && c >= a)
+    Console.WriteLine($"Большее число : {c}");
